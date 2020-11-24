3dc98cf633ddda05c9a3eef4919dd42bb4ef1359
Perry <E096752@wv.gov>
2020-11-24 12:36:22 -0500

    All sorting, filtering, pagination workinggit add *

--- Controllers/InventoriesController.cs
+++ Controllers/InventoriesController.cs
@@ -7,7 +7,10 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using PartsInventoryV6;
+using System.Threading.Tasks;
+using PagedList;
 
+//File was made with the help of: https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/getting-started-with-ef-using-mvc/sorting-filtering-and-paging-with-the-entity-framework-in-an-asp-net-mvc-application
 namespace PartsInventoryV6.Controllers
 {
     public class InventoriesController : Controller
@@ -15,9 +18,61 @@ namespace PartsInventoryV6.Controllers
         private EquipmentContext db = new EquipmentContext();
 
         // GET: Inventories
-        public ActionResult Index()
+        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            return View(db.Inventories.ToList());
+            // using System;
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.DescSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.NewNumSort = sortOrder == "newNum" ? "newNum_desc" : "newNum";
+            ViewBag.OldNumSort = sortOrder == "oldNum" ? "oldNum_desc" : "oldNum";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var parts = from part in db.Inventories
+                        select part;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                parts = parts.Where(part => part.DESCRIPTION.Contains(searchString)
+                                       || part.OLD_NUMBER.Contains(searchString)
+                                       || part.NEW_NUMBER.ToString().Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc": //Sort by name descending
+                    parts = parts.OrderByDescending(part => part.DESCRIPTION);
+                    break;
+                case "newNum": //Sort by new number aescending
+                    parts = parts.OrderBy(part => part.NEW_NUMBER);
+                    break;
+                case "newNum_desc": //Sort by new number descending
+                    parts = parts.OrderByDescending(part => part.NEW_NUMBER);
+                    break;
+                case "oldNum": //Sort by old number
+                    parts = parts.OrderBy(part => part.OLD_NUMBER);
+                    break;
+                case "oldNum_desc": //Sort by old number descending
+                    parts = parts.OrderByDescending(part => part.OLD_NUMBER);
+                    break;
+                default: //Default to ordering by part ID
+                    parts = parts.OrderBy(part => part.ID);
+                    break;
+
+            }
+
+            int pageSize = 100;
+            int pageNumber = (page ?? 1);
+            return View(parts.ToPagedList(pageNumber, pageSize));
         }
 
         // GET: Inventories/Details/5
